79950d1a916d4e39e3aad794677d75da12b635b0
allain <allain.lalonde@favequest.com>
2017-09-04 21:40:15 -0400

    converting to node 8 and using standard + eslint after tests pass

--- examples/example_read_level.js
+++ examples/example_read_level.js
@@ -1,10 +1,10 @@
-var readLevel=require("./../index").level.readLevel;
+const readLevel = require('./../index').level.readLevel
 
-if(process.argv.length !=3) {
-  console.log("Usage : node example_read_level.js <level.dat>");
-  process.exit(1);
+if (process.argv.length !== 3) {
+  console.log('Usage : node example_read_level.js <level.dat>')
+  process.exit(1)
 }
 
 readLevel(process.argv[2])
-.then(function(nbt){console.log(JSON.stringify(nbt,null,2))})
-.catch(function(err){console.log(err.stack)});
+  .then(function (nbt) { console.log(JSON.stringify(nbt, null, 2)) })
+  .catch(function (err) { console.log(err.stack) })
--- examples/example_write_level.js
+++ examples/example_write_level.js
@@ -1,9 +1,9 @@
-var writeLevel=require("./../index").level.writeLevel;
+const writeLevel = require('./../index').level.writeLevel
 
-if(process.argv.length !=3) {
-  console.log("Usage : node example_write_level.js <level.dat>");
-  process.exit(1);
+if (process.argv.length !== 3) {
+  console.log('Usage : node example_write_level.js <level.dat>')
+  process.exit(1)
 }
 
-writeLevel(process.argv[2],{"RandomSeed":[123,0]})
-  .catch(function(err){console.log(err.stack)});
+writeLevel(process.argv[2], {'RandomSeed': [123, 0]})
+  .catch(function (err) { console.log(err.stack) })
