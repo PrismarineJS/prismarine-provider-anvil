0308af9410c295b9d2ed6591b543053e222d5ecd
Vitaly <vital2580@icloud.com>
2023-08-30 06:13:04 +0300

    require lazily

--- src/chunk.js
+++ src/chunk.js
@@ -1,20 +1,29 @@
+const PrismarineChunk = require('prismarine-chunk')
+
 module.exports = (registryOrVersion) => {
   const registry = typeof registryOrVersion === 'string' ? require('prismarine-registry')(registryOrVersion) : registryOrVersion
-  const Chunk = require('prismarine-chunk')(registry)
+  const Chunk = PrismarineChunk(registry)
 
   const chunkImplementations = {
-    1.8: require('./1.8/chunk'),
-    1.9: require('./1.8/chunk'),
-    '1.10': require('./1.8/chunk'),
-    1.11: require('./1.8/chunk'),
-    1.12: require('./1.8/chunk'),
-    1.13: require('./1.13/chunk'),
-    1.14: require('./1.14/chunk')('1.14', 1976),
-    1.15: require('./1.14/chunk')('1.15', 2230),
-    1.16: require('./1.14/chunk')('1.16', 2567, true),
-    1.17: require('./1.14/chunk')('1.17', 2730, true),
-    1.18: require('./1.18/chunk')
+    1.8: () => require('./1.8/chunk'),
+    1.9: () => require('./1.8/chunk'),
+    '1.10': () => require('./1.8/chunk'),
+    1.11: () => require('./1.8/chunk'),
+    1.12: () => require('./1.8/chunk'),
+    1.13: () => require('./1.13/chunk'),
+    1.14: () => require('./1.14/chunk')('1.14', 1976),
+    1.15: () => require('./1.14/chunk')('1.15', 2230),
+    1.16: () => require('./1.14/chunk')('1.16', 2567, true),
+    1.17: () => require('./1.14/chunk')('1.17', 2730, true),
+    1.18: () => require('./1.18/chunk')
   }
 
-  return chunkImplementations[registry.version.majorVersion](Chunk, registry)
+  const loadVersion = registry.version.majorVersion
+  const implementationLoader = chunkImplementations[loadVersion]?.()
+
+  if (!implementationLoader) {
+    throw new Error(`Unsupported version: ${loadVersion}`)
+  }
+
+  return implementationLoader(Chunk, registry)
 }
