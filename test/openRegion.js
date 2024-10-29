6b0517ef64623d031b6dfbcd422bf0149d9cacc0
Romain Beaumont <romain.rom1@gmail.com>
2024-10-29 22:56:58 +0100

    Fix test versions. (#80)
    * Fix test versions.
    
    * Reimplement cycle test.
    
    Doesn't use buffer equality as different buffers may still represent the same blocks.

--- test/openRegion.js
+++ test/openRegion.js
@@ -3,12 +3,13 @@ const fs = require('fs')
 const path = require('path')
 const assert = require('assert')
 const fixtures = path.join(__dirname, 'fixtures')
-const versions = fs.readdirSync(fixtures)
+const prismarineProviderAnvil = require('prismarine-provider-anvil')
+const testedVersions = prismarineProviderAnvil.testedVersions
 describe('can get chunks from region files', function () {
   this.timeout(5 * 1000)
-  for (const version of versions) {
+  for (const version of testedVersions) {
     it(`works for ${version}`, async () => {
-      const Anvil = require('prismarine-provider-anvil').Anvil(version)
+      const Anvil = prismarineProviderAnvil.Anvil(version)
       const filePath = path.join(fixtures, version)
       const anvil = new Anvil(filePath)
       const [, xStr, zStr] = fs.readdirSync(filePath)[0].match(/r\.(-?\d+)\.(-?\d+)\.mca/)
